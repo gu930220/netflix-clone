ec1b7f3949b2c0e9e045a9e23917e01b0e0ccbc7
ikmo <gu930220@hanmail.net>
2020-08-18 01:32:59 +0900

    200818 fetch data

---
+++ src/Row.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs based on a specific condition/varaible
+
+  useEffect(() => {
+    // if [], run once when the row loads, and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
+      // https://api.themoviedb.org/3
+      // /discover/movie?api_key=${API_KEY}&with_genres=28
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {/* several row__poster(s) */}
+
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
